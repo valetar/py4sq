b4a4a2fc8002832d5a3f536ba3b94f82951cef7a
Alexander Valet <valetar@gmail.com>
2012-01-22 18:47:58 +0100

    Fix link in readme

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='py4sq',
+      version='0.1',
+      description='Foursquare API Library',
+      author='Alexander Valet',
+      author_email='valetar@googlemail.com',
+      packages=['py4sq',],
+      url = "https://github.com/valetar/py4sq/archives/master",
+      download_url="http://github.com/valetar/py4sq/archives/master",
+      keywords=["chef", "devops", "operations", "sysadmin"],
+      long_description='An unofficial wrapper for Foursquare APIv2',
+      license='Modified BSD',
+
+     )
